57fde9054359d1b1ad0c39411663e4d288a2f955
mathieudutour <mathieu@dutour.me>
2019-08-08 08:20:30 +0200

    fix lint

--- index.js
+++ index.js
@@ -1,55 +1,59 @@
-var toObject = require('util').toObject
+var toObject = require("util").toObject;
 
 var currentLocale = (function currentLocale() {
   // NSLocale.currentLocale() only returns the language that is supported by the host application
-  var languageCode = String(NSLocale.preferredLanguages()[0]).split('-')[0]
+  var languageCode = String(NSLocale.preferredLanguages()[0]).split("-")[0];
   if (!languageCode) {
-    return undefined
+    return undefined;
   }
-  var countryCode = String(NSLocale.currentLocale().localeIdentifier()).split('_')[1]
+  var countryCode = String(NSLocale.currentLocale().localeIdentifier()).split(
+    "_"
+  )[1];
   if (!countryCode) {
-    return languageCode
+    return languageCode;
   }
-  return languageCode + '-' + countryCode
-})()
+  return languageCode + "-" + countryCode;
+})();
 
 module.exports = {
-  get title()  {
-    return String(__command.name())
+  get title() {
+    return String(__command.name());
   },
-  get version()  {
-    var pluginBundle = __command.pluginBundle()
-    return pluginBundle ? String(pluginBundle.version()) : '0.0.0'
+  get version() {
+    var pluginBundle = __command.pluginBundle();
+    return pluginBundle ? String(pluginBundle.version()) : "0.0.0";
   },
   get versions() {
-    var pluginBundle = __command.pluginBundle()
+    var pluginBundle = __command.pluginBundle();
     return {
-      plugin: pluginBundle ? String(pluginBundle.version()) : '0.0.0',
+      plugin: pluginBundle ? String(pluginBundle.version()) : "0.0.0",
       sketch: MSApplicationMetadata.metadata().appVersion
-    }
+    };
   },
   arch: "x64",
   platform: "darwin",
   cwd() {
-    var pluginBundle = __command.pluginBundle()
-    return pluginBundle ? String(pluginBundle.url().path()) : '/tmp'
+    var pluginBundle = __command.pluginBundle();
+    return pluginBundle ? String(pluginBundle.url().path()) : "/tmp";
   },
   get env() {
-    var env = toObject(NSProcessInfo.processInfo().environment())
-    var pluginBundle = __command.pluginBundle()
-    env.command = __command
+    var env = toObject(NSProcessInfo.processInfo().environment());
+    var pluginBundle = __command.pluginBundle();
+    env.command = __command;
     if (pluginBundle) {
-      env.plugin = pluginBundle
+      env.plugin = pluginBundle;
     }
     if (currentLocale) {
-      env.LANG = currentLocale
+      env.LANG = currentLocale;
     }
-    return env
+    return env;
   },
   get pid() {
-    return __command.identifier
+    return __command.identifier;
   },
   execPath: String(NSBundle.mainBundle().executablePath()),
   type: "sketch",
-  nextTick: typeof setImmediate !== 'undefined' ? setImmediate : undefined
-}
+  get nextTick() {
+    return setImmediate;
+  }
+};
